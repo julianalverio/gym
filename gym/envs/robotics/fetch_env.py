e285e5282e0b10e17df565435a5dc7eda1cb2240
julianalverio <julian.a.alverio@gmail.com>
2019-07-30 12:13:24 -0400

    iterating

--- gym/envs/robotics/fetch_env.py
+++ gym/envs/robotics/fetch_env.py
@@ -180,7 +180,9 @@ class FetchEnv(robot_env.RobotEnv):
         import pdb; pdb.set_trace()
         sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()
         site_id = self.sim.model.site_name2id('target0')
-        self.sim.model.site_pos[site_id] = self.goal - sites_offset[0]
+        self.sim.model.site_pos[site_id] = np.array([50., 50., 50.])
+
+        # self.sim.model.site_pos[site_id] = self.goal - sites_offset[0]
         self.sim.forward()
 
     def _reset_sim(self):
