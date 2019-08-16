d367930cbe084556c66b6d46c73a55b8521168b9
julianalverio <julian.a.alverio@gmail.com>
2019-08-16 15:54:20 -0400

    iterating

--- gym/envs/robotics/robot_env.py
+++ gym/envs/robotics/robot_env.py
@@ -1,10 +1,18 @@
-import os
 import copy
+from gym.envs.robotics import rotations, robot_env, utils
 import numpy as np
+import requests
+import os
+import cv2
+
+import sys
+sys.path.insert(0, '/storage/jalverio/sentence-tracker/st')
 
 import gym
 from gym import error, spaces
 from gym.utils import seeding
+import pickle
+from cv2 import VideoWriter, VideoWriter_fourcc
 
 try:
     import mujoco_py
@@ -15,22 +23,22 @@ DEFAULT_SIZE = 500
 
 
 class RobotEnv(gym.GoalEnv):
-    def __init__(self, model_path, initial_qpos, n_actions, n_substeps, reward_type):
-        if model_path.startswith('/'):
-            fullpath = model_path
-        else:
-            fullpath = os.path.join(os.path.dirname(__file__), 'assets', model_path)
-        if not os.path.exists(fullpath):
-            raise IOError('File {} does not exist'.format(fullpath))
+    def __init__(self, gripper_extra_height, block_gripper, has_object, target_in_the_air, target_offset, obj_range,
+                 target_range, distance_threshold, reward_type, model_path, n_substeps, n_actions, initial_qpos):
+        self.gripper_extra_height = gripper_extra_height
+        self.block_gripper = block_gripper
+        self.has_object = has_object
+        self.target_in_the_air = target_in_the_air
+        self.target_offset = target_offset
+        self.obj_range = obj_range
+        self.target_range = target_range
+        self.distance_threshold = distance_threshold
+        self.reward_type = reward_type
 
+        fullpath = os.path.join(os.path.dirname(__file__), 'assets', model_path)
         model = mujoco_py.load_model_from_path(fullpath)
         self.sim = mujoco_py.MjSim(model, nsubsteps=n_substeps)
         self.viewer = None
-
-        self.metadata = {
-            'render.modes': ['human', 'rgb_array'],
-            'video.frames_per_second': int(np.round(1.0 / self.dt))
-        }
 
         self.seed()
         self._env_setup(initial_qpos=initial_qpos)
@@ -44,13 +52,15 @@ class RobotEnv(gym.GoalEnv):
             achieved_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
             observation=spaces.Box(-np.inf, np.inf, shape=obs['observation'].shape, dtype='float32'),
         ))
-        self.frames = []
-        self.reward_type = reward_type
         self._viewers = {
             'human': None,
             'rgb_array': None
         }
-        self.render(mode='human')
+        self.trajectory = []
+        self.frames = []
+        self.url = 'http://baffin.csail.mit.edu:5000'
+        self.save_idx = 0
+
 
     @property
     def dt(self):
@@ -77,6 +87,7 @@ class RobotEnv(gym.GoalEnv):
 
         if self.reward_type == 'visual':
             self.frames.append(self.render(mode='rgb_array'))
+            self.trajectory.append(action)
         # prefix = '/storage/jalverio/robot_images/incomplete_track_videos/'
         # next_idx = max([int(x.replace('.npy', '')) for x in os.listdir(prefix)]) + 1
         # np.save(prefix + str(next_idx), np.array(self.frames))
@@ -96,31 +107,67 @@ class RobotEnv(gym.GoalEnv):
             did_reset_sim = self._reset_sim()
         self.goal = self._sample_goal().copy()
         obs = self._get_obs()
+        self.frames = []
+        self.trajectory = []
         return obs
+
+    def save(self):
+        idx = self.save_idx
+        prefix = '/storage/jalverio/robot_images/successes/' + idx + '/'
+        os.mkdir(prefix)
+        print('saving success to:', prefix)
+        with open(os.path.join(prefix, idx, 'frames'), 'wb') as pickle_file:
+            pickle.dump(self.frames, pickle_file)
+        with open(os.path.join(prefix, idx, 'actions'), 'wb') as pickle_file:
+            pickle.dump(self.trajectory, pickle_file)
+        self.write_video(prefix)
+        self.save_idx += 1
+
+    def write_video(frames, location):
+        FPS = 5.0
+        height, width = frames[0].shape[:2]
+        fourcc = VideoWriter_fourcc(*'avc1')
+        video = VideoWriter('%s/fetch.avi' % location, 0, FPS, (width, height))
+        for frame in frames:
+            video.write(frame)
+        video.release()
 
     def close(self):
         if self.viewer is not None:
             self.viewer.finish()
             self.viewer = None
 
-    # def render(self, mode='human'):
-    #     self._render_callback()
-    #     if mode == 'rgb_array':
-    #         self._get_viewer().render()
-    #         # window size used for old mujoco-py:
-    #         width, height = 500, 500
-    #         data = self._get_viewer().read_pixels(width, height, depth=False)
-    #         # original image is upside-down, so flip it
-    #         return data[::-1, :, :]
-    #     elif mode == 'human':
-    #         self._get_viewer().render()
-    #
-    # def _get_viewer(self):
-    #     if self.viewer is None:
-    #         self.viewer = mujoco_py.MjViewer(self.sim)
-    #         self._viewer_setup()
-    #     return self.viewer
+    def compute_reward(self, achieved_goal, goal, info):
+        # Compute distance between goal and the achieved goal.
+        d = np.linalg.norm(achieved_goal - goal, axis=-1)
+        if self.reward_type == 'sparse':
+            return -(d > self.distance_threshold).astype(np.float32)
+        elif self.reward_type == 'visual':
+            if len(self.frames) <= 8:
+                return np.float32(0.)
 
+            frames = np.array(self.sample_frames(self.frames))
+            data = {'images': frames.tolist()}
+            result = requests.post(self.url, json=data)
+            reward = float(result.text)
+            if reward == 1.:
+                self.save()
+            if reward == -1:
+                print('INCOMPLETE TRACK EXCEPTION')
+                reward = 0
+            return np.float32(reward)
+
+        else:
+            return -d
+
+    def sample_frames(self, frames, n=8):
+        bin_width = int(len(frames) / n - 1)
+        idxs = bin_width * np.array(list(range(n)))
+        sampled_frames = []
+        for idx in idxs:
+            sampled_frames.append(frames[idx])
+        assert len(sampled_frames) == n
+        return sampled_frames
 
     def render(self, mode='human', width=DEFAULT_SIZE, height=DEFAULT_SIZE):
         self._render_callback()
@@ -148,55 +195,127 @@ class RobotEnv(gym.GoalEnv):
     # ----------------------------
 
     def _reset_sim(self):
-        """Resets a simulation and indicates whether or not it was successful.
-        If a reset was unsuccessful (e.g. if a randomized state caused an error in the
-        simulation), this method should indicate such a failure by returning False.
-        In such a case, this method will be called again to attempt a the reset again.
-        """
         self.sim.set_state(self.initial_state)
+
+        # Randomize start position of object.
+        if self.has_object:
+            object_xpos = self.initial_gripper_xpos[:2]
+            while np.linalg.norm(object_xpos - self.initial_gripper_xpos[:2]) < 0.1:
+                object_xpos = self.initial_gripper_xpos[:2] + self.np_random.uniform(-self.obj_range, self.obj_range, size=2)
+            object_qpos = self.sim.data.get_joint_qpos('object0:joint')
+            assert object_qpos.shape == (7,)
+            object_qpos[:2] = object_xpos
+            self.sim.data.set_joint_qpos('object0:joint', object_qpos)
+
         self.sim.forward()
         return True
 
     def _get_obs(self):
-        """Returns the observation.
-        """
-        raise NotImplementedError()
+        # positions
+        grip_pos = self.sim.data.get_site_xpos('robot0:grip')
+        dt = self.sim.nsubsteps * self.sim.model.opt.timestep
+        grip_velp = self.sim.data.get_site_xvelp('robot0:grip') * dt
+        robot_qpos, robot_qvel = utils.robot_get_obs(self.sim)
+        if self.has_object:
+            object_pos = self.sim.data.get_site_xpos('object0')
+            # rotations
+            object_rot = rotations.mat2euler(self.sim.data.get_site_xmat('object0'))
+            # velocities
+            object_velp = self.sim.data.get_site_xvelp('object0') * dt
+            object_velr = self.sim.data.get_site_xvelr('object0') * dt
+            # gripper state
+            object_rel_pos = object_pos - grip_pos
+            object_velp -= grip_velp
+        else:
+            object_pos = object_rot = object_velp = object_velr = object_rel_pos = np.zeros(0)
+        gripper_state = robot_qpos[-2:]
+        gripper_vel = robot_qvel[-2:] * dt  # change to a scalar if the gripper is made symmetric
+
+        if not self.has_object:
+            achieved_goal = grip_pos.copy()
+        else:
+            achieved_goal = np.squeeze(object_pos.copy())
+        obs = np.concatenate([
+            grip_pos, object_pos.ravel(), object_rel_pos.ravel(), gripper_state, object_rot.ravel(),
+            object_velp.ravel(), object_velr.ravel(), grip_velp, gripper_vel,
+        ])
+
+        return {
+            'observation': obs.copy(),
+            'achieved_goal': achieved_goal.copy(),
+            'desired_goal': self.goal.copy(),
+        }
 
     def _set_action(self, action):
-        """Applies the given action to the simulation.
-        """
-        raise NotImplementedError()
+        assert action.shape == (4,)
+        action = action.copy()  # ensure that we don't change the action outside of this scope
+        pos_ctrl, gripper_ctrl = action[:3], action[3]
+
+        pos_ctrl *= 0.05  # limit maximum change in position
+        rot_ctrl = [1., 0., 1., 0.]  # fixed rotation of the end effector, expressed as a quaternion
+        gripper_ctrl = np.array([gripper_ctrl, gripper_ctrl])
+        assert gripper_ctrl.shape == (2,)
+        if self.block_gripper:
+            gripper_ctrl = np.zeros_like(gripper_ctrl)
+        action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
+
+        # Apply action to simulation.
+        utils.ctrl_set_action(self.sim, action)
+        utils.mocap_set_action(self.sim, action)
 
     def _is_success(self, achieved_goal, desired_goal):
-        """Indicates whether or not the achieved goal successfully achieved the desired goal.
-        """
-        raise NotImplementedError()
+        d = np.linalg.norm(achieved_goal - desired_goal, axis=-1)
+        return (d < self.distance_threshold).astype(np.float32)
 
     def _sample_goal(self):
-        """Samples a new goal and returns it.
-        """
-        raise NotImplementedError()
+        if self.has_object:
+            goal = self.initial_gripper_xpos[:3] + self.np_random.uniform(-self.target_range, self.target_range, size=3)
+            goal += self.target_offset
+            goal[2] = self.height_offset
+            if self.target_in_the_air and self.np_random.uniform() < 0.5:
+                goal[2] += self.np_random.uniform(0, 0.45)
+        else:
+            goal = self.initial_gripper_xpos[:3] + self.np_random.uniform(-0.15, 0.15, size=3)
+        return goal.copy()
 
     def _env_setup(self, initial_qpos):
-        """Initial configuration of the environment. Can be used to configure initial state
-        and extract information from the simulation.
-        """
-        pass
+        for name, value in initial_qpos.items():
+            self.sim.data.set_joint_qpos(name, value)
+        utils.reset_mocap_welds(self.sim)
+        self.sim.forward()
+
+        # Move end effector into position.
+        gripper_target = np.array([-0.498, 0.005, -0.431 + self.gripper_extra_height]) + self.sim.data.get_site_xpos('robot0:grip')
+        gripper_rotation = np.array([1., 0., 1., 0.])
+        self.sim.data.set_mocap_pos('robot0:mocap', gripper_target)
+        self.sim.data.set_mocap_quat('robot0:mocap', gripper_rotation)
+        for _ in range(10):
+            self.sim.step()
+
+        # Extract information for sampling goals.
+        self.initial_gripper_xpos = self.sim.data.get_site_xpos('robot0:grip').copy()
+        if self.has_object:
+            self.height_offset = self.sim.data.get_site_xpos('object0')[2]
 
     def _viewer_setup(self):
-        """Initial configuration of the viewer. Can be used to set the camera position,
-        for example.
-        """
-        pass
+        self.viewer.cam.lookat[0] = 1.34184371
+        self.viewer.cam.lookat[1] = 0.74910048
+        self.viewer.cam.lookat[2] = 0.53471723
+        self.viewer.cam.azimuth = 180.
+        self.viewer.cam.elevation = 0.
+        self.viewer.cam.distance = 0.8
 
     def _render_callback(self):
-        """A custom callback that is called before rendering. Can be used
-        to implement custom visualizations.
-        """
-        pass
+        # Visualize target.
+        # sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()
+        site_id = self.sim.model.site_name2id('target0')
+        self.sim.model.site_pos[site_id] = np.array([50., 50., 50.])
+
+        # self.sim.model.site_pos[site_id] = self.goal - sites_offset[0]
+        self.sim.forward()
 
     def _step_callback(self):
-        """A custom callback that is called after stepping the simulation. Can be used
-        to enforce additional constraints on the simulation state.
-        """
-        pass
+        if self.block_gripper:
+            self.sim.data.set_joint_qpos('robot0:l_gripper_finger_joint', 0.)
+            self.sim.data.set_joint_qpos('robot0:r_gripper_finger_joint', 0.)
+            self.sim.forward()
