bc19ef6385fbe04b17c1ef945e3dfc0da3856ba3
julianalverio <julian.a.alverio@gmail.com>
2019-10-20 12:55:13 -0400

    iterating

--- gym/envs/robotics/robot_env.py
+++ gym/envs/robotics/robot_env.py
@@ -63,7 +63,7 @@ class RobotEnv(gym.GoalEnv):
         }
         self.trajectory = []
         self.frames = []
-        self.url = 'http://baffin.csail.mit.edu:5000'
+        self.url = 'http://melville.csail.mit.edu:5000'
         self.save_idx = 0
 
 
