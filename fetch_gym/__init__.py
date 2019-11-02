a0f30f26474201946f599bfe856ebab8f175d313
julianalverio <julian.a.alverio@gmail.com>
2019-11-01 20:32:10 -0400

    iterating

--- fetch_gym/__init__.py
+++ fetch_gym/__init__.py
@@ -1,15 +1,17 @@
+import fetch_gym as gym
+
 import distutils.version
 import os
 import sys
 import warnings
 
 from gym import error
-from fetch_gym.utils import reraise
+from gym.utils import reraise
 from gym.version import VERSION as __version__
 
-from fetch_gym.core import Env, GoalEnv, Space, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper
-from fetch_gym.envs import make, spec
-from fetch_gym import wrappers, spaces, logger
+from gym.core import Env, GoalEnv, Space, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper
+from gym.envs import make, spec
+from gym import wrappers, spaces, logger
 
 def undo_logger_setup():
     warnings.warn("gym.undo_logger_setup is deprecated. gym no longer modifies the global logging configuration")
